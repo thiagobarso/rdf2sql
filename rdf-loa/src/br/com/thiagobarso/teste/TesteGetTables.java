f56bd51629997f5e719d1649ae7cbcb084cf5bf7
thiagosoares <thiagosoares@thiagosoares-HP-Compaq-Elite-8300-SFF>
2015-06-10 17:29:12 -0300

    teste função gettables

---
+++ rdf-loa/src/br/com/thiagobarso/teste/TesteGetTables.java
@@ -0,0 +1,15 @@
+package br.com.thiagobarso.teste;
+
+import java.util.ArrayList;
+
+import br.com.thiagobarso.service.SearchInRdf;
+
+public class TesteGetTables {
+
+	public static void main(String[] args) {
+		String singleroot = "/rdf-timoteo/loa2015/loa2015.nt";
+		System.out.println(new SearchInRdf().getTables(singleroot).toString());
+
+	}
+
+}
