8e0721940690955c2208122553c9485638f5491d
thiagobarso <thiagosoares@gmail.com>
2015-06-27 17:49:44 -0300

    ajustes

--- rdf-loa/src/br/com/thiagobarso/system/ConnectionFactory.java
+++ rdf-loa/src/br/com/thiagobarso/system/ConnectionFactory.java
@@ -1,22 +1,39 @@
 package br.com.thiagobarso.system;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
 	public Connection getConnection() {
+		String banco = null;
+		String usuario = null;
+		String senha = null;
+		String host = null;
+		String porta = null;
+
 		try {
 			Properties prop = DoRdf.getProp();
-			String banco = prop.getProperty("prop.server.banco");
-			String usuario = prop.getProperty("prop.server.usuario");
-			String senha = prop.getProperty("prop.server.senha");
-			String host = prop.getProperty("prop.server.host");
-			String porta = prop.getProperty("prop.server.porta");
-			return DriverManager.getConnection("jdbc:postgresql://"+host+":"+ porta +"/" + banco,
-					usuario, senha);
-		} catch (Exception e) {
-			System.out.println("Ops!Algo deu errado com a conexao da aplciação com o banco de dados!");
+			banco = prop.getProperty("prop.server.banco");
+			usuario = prop.getProperty("prop.server.usuario");
+			senha = prop.getProperty("prop.server.senha");
+			host = prop.getProperty("prop.server.host");
+			porta = prop.getProperty("prop.server.porta");
+		} catch (IOException e) {
+			System.out
+					.println("Ops!Algo deu errado com o nome da localização do arquivo rdf! ");
+			System.out.println("Reveja o arquivo dados.properties na pasta desse jar.");
+			e.printStackTrace();
+		}
+
+		try {
+			return DriverManager.getConnection("jdbc:postgresql://" + host
+					+ ":" + porta + "/" + banco, usuario, senha);
+		} catch (SQLException e) {
+			System.out
+					.println("Ops!Algo deu errado com a conexao da aplicação com o banco de dados!");
 			throw new RuntimeException(e);
 		}
 	}
