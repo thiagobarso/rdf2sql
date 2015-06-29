a7e746e92b99a55be7a5b035f870156c11d19838
thiagobarso <thiagosoares@gmail.com>
2015-06-29 10:54:08 -0300

    comentando classe de teste.

--- rdf-loa/src/br/com/thiagobarso/teste/TesteGetQuerySelectRdf.java
+++ rdf-loa/src/br/com/thiagobarso/teste/TesteGetQuerySelectRdf.java
@@ -1,34 +1,19 @@
 package br.com.thiagobarso.teste;
 
-public class TesteGetQuerySelectRdf {
+/*
+import java.util.ArrayList;
+import br.com.thiagobarso.service.SearchInRdf;
+*/
 
-	/*public static void main(String[] args) {
-		String singleroot = "/rdf-timoteo/loa2015/loa2015.nt";
-		String t = "loa:ItemDespesa";
-		ArrayList<String> colunas = new ArrayList<String>();
-		colunas.add("loa:temIdentificadorUso");
-		colunas.add("loa:valorLeiMaisCredito");		
-		colunas.add("loa:temModalidadeAplicacao");
-		colunas.add("loa:temUnidadeOrcamentaria");
-		colunas.add("loa:temSubfuncao");
-		colunas.add("loa:valorLiquidado");
-		colunas.add("loa:temFuncao");
-		colunas.add("loa:valorDotacaoInicial");
-		colunas.add("loa:temPrograma");
-		colunas.add("loa:temFonteRecursos");
-		colunas.add("loa:temGND");
-		colunas.add("loa:temCategoriaEconomica");
-		colunas.add("loa:temElementoDespesa");
-		colunas.add("loa:valorEmpenhado");
-		colunas.add("loa:temPlanoOrcamentario");
-		colunas.add("loa:valorProjetoLei");
-		colunas.add("loa:valorPago");
-		colunas.add("loa:temResultadoPrimario");
-		colunas.add("loa:temAcao");
-		colunas.add("loa:temExercicio");
-		colunas.add("loa:temEsfera");
-		colunas.add("loa:temSubtitulo");
-		String teste = new SearchInRdf().getQuerySelectRdf(t, singleroot,
-				colunas);		
+public class TesteGetQuerySelectRdf {
+/*
+	private static SearchInRdf search = new SearchInRdf();
+
+	public static void main(String[] args) {
+		String root = search.getArquivoRdf();
+		String tabela = "loa:Acao";
+		ArrayList<String> colunasPertencentesATabela = search.getColunas(tabela,
+				root);
+		search.getQuerySelectRdf(tabela, root, colunasPertencentesATabela);				
 	}*/
 }
